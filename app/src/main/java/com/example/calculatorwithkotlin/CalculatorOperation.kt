608971ea7bdcf183312bdffa1335137e17839c6b
Lenora Stevens <114180320+lenorastevens@users.noreply.github.com>
2024-02-25 17:34:22 -0600

    Initial commit

---
+++ app/src/main/java/com/example/calculatorwithkotlin/CalculatorOperation.kt
@@ -0,0 +1,9 @@
+package com.example.calculatorwithkotlin
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add: CalculatorOperation(symbol = "+")
+    object Subtract: CalculatorOperation(symbol = "-")
+    object Multiply: CalculatorOperation(symbol = "x")
+    object Divide: CalculatorOperation(symbol = "/")
+
+}
